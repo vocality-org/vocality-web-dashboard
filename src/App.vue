4abb50dcdd648ba61277c651bea294ae2f6eade6
Julian Kaindl <kaindl745@gmail.com>
2019-09-03 18:54:57 +0200

    Add login page

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,5 @@
 <template>
-    <v-app>
-        <v-app-bar app>
-            <v-toolbar-title class="headline text-uppercase">
-                <span>Vuetify</span>
-                <span class="font-weight-light">MATERIAL DESIGN</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-                <span class="mr-2">Latest Release</span>
-            </v-btn>
-        </v-app-bar>
-
-        <v-content>
-            <HelloWorld />
-        </v-content>
-    </v-app>
+    <router-view></router-view>
 </template>
 
 <script lang="ts">
@@ -30,3 +15,11 @@ import HelloWorld from './components/HelloWorld.vue';
 })
 export default class App extends Vue {}
 </script>
+
+<style lang="scss">
+html {
+    font-family: 'Roboto', sans-serif;
+    font-weight: normal;
+    overflow: auto !important;
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,18 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import Login from '@/views/Login.vue';
+
 Vue.use(Router);
 
 export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes: [],
+    routes: [
+        {
+            path: '/',
+            name: 'index',
+            component: Login,
+        },
+    ],
 });
